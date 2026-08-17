aaefbc6bdcf402461fb52c78cb39fb50b7a9381a
paulor2ff <paulo.r2ff@gmail.com>
2026-08-17 11:17:05 -0300

    Exercises

---
+++ introduction_to_java_programming/chapter_01/ProgrammingExercisesDisplayFiveMessages.java
@@ -0,0 +1,9 @@
+public class ProgrammingExercisesDisplayFiveMessages {
+    public static void main(String[] args) {
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+    }
+}
---
+++ introduction_to_java_programming/chapter_01/ProgrammingExercisesDisplayThreeMessages.java
@@ -0,0 +1,7 @@
+public class ProgrammingExercisesDisplayThreeMessages {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java Now");
+        System.out.println("Programming is fun");
+    }
+}
